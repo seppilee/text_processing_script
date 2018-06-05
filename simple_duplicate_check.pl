c74a0d77eb430eb9465944dfda9e44340a0426ff
cuba <seppi.lee@hanmail.net>
2018-06-05 23:12:23 +0900

    simple check

--- simple_duplicate_check.pl
+++ simple_duplicate_check.pl
@@ -1,21 +1,25 @@
 #!/usr/bin/perl
-
 my %names;
+my $Eng;
 #the first file
 while(<>){
 	$_ =~ s/\x{FEFF}|\r//g;
-	$_ =~ s/\.|\?|\!|,|\"|\'|\s+//g;
+	#$_ =~ m/(.+)\t(.+)/;
+	#$Eng = $1;
+	$_ =~ s/\.|\?|\!|,|\"|\'|\s+|:|;|-//g;
 	$names{$_} = 1;
 		last if eof;
 }
 
 # scan second file
+
 while(<>){
    $_ =~ s/\x{FEFF}|\r//g;
    my $base = $_;
-   $_ =~ s/\.|\?|\!|,|\"|\'|\s+//g;
-   print "$_ \t $base" if $names{$_};
-					}
+   $_ =~ s/\.|\?|\!|,|\"|\'|\s+|:|;|-//g;
+   
+   print "$base" if $names{$_} != 1; 		#print not dulplicated list from first file to second file
+	}
 
 
 
