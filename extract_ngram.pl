eda8698729043d6d2fa3d36201e74bf27dd2bce5
cuba <seppi.lee@hanmail.net>
2018-06-09 23:53:33 +0900

    char ngram

--- extract_ngram.pl
+++ extract_ngram.pl
@@ -16,6 +16,7 @@ sub trim {
 }
 
 $#ARGV == 1 or die "Usage: $0 ngram_size filename\n";
+
 $ARGV[0] =~ m/^[1-9][0-9]*$/ or die "Error: invalid ngram size\n";
 open my $fo, $ARGV[1] or die "Error: $ARGV[1] not found\n";
 
@@ -23,15 +24,17 @@ my $line = 0;
 my %ngrams;
   
 while (<$fo>) {
-   $_ = trim $_;
-   my @tkn = split / /;
-   for my $i (0 .. $ARGV[0] - 1) {
-       for my $j (0 .. $#tkn - $i) {
-	   $ngrams{join ' ', @tkn[$j .. $j + $i]}++;
-			}
+	$_ = trim $_;
+	my $len = length($_) - 1;
+	for my $i (0 .. $ARGV[0] - 1) {
+		for my $j (0 .. $len - $i) {
+			next if substr($_, $j, 1) eq ' ' or substr($_, $i + $j, 1) eq ' ';
+			$ngrams{substr $_, $j, $i + 1}++;
 		}
-		warn "$ARGV[1]: $line lines hashed\n" if ++$line % 100000 == 0;
 	}
+	warn "$ARGV[2]: $line lines hashed\n" if ++$line % 100000 == 0;
+	}
+
 
 $line = 0;
 foreach (sort {$ngrams{$b} <=> $ngrams{$a}} keys %ngrams) {
